994327169e5c2cc7ccac927b8674347ff8ec153d
Larose <imdev242@gmail.com>
2023-09-08 21:29:35 +0000

    modification du fichier server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-//Install express server
+/* //Install express server
 const express=require('express');
 const path=require('path');
 
@@ -13,13 +13,13 @@ app.get('/*',(req,res)=> res.sendFile('index.html',{root:'dist/angular-managemen
 
 //Start the app by listening  on the default heroku port
 app.listen(process.env.PORT || 8080);
+ */
 
-
-/* const express = require('express');
+const express = require('express');
 const path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/dist/angular-management-of-schools'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
 '/dist/angular-management-of-schools/index.html'));});
-app.listen(process.env.PORT || 8080); */
+app.listen(process.env.PORT || 8080);
